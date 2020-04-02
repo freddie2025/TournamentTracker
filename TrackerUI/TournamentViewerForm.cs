2c1bb4f0bb865d99f05961f2fb6f6ecd1949a2c9
Fred James <fredjsam@live.co.uk>
2020-04-02 10:33:54 +0100

    Add Basic Error Handling

--- TrackerUI/TournamentViewerForm.cs
+++ TrackerUI/TournamentViewerForm.cs
@@ -149,8 +149,46 @@ namespace TrackerUI
 			LoadMatchups((int)roundDropDown.SelectedItem); 
 		}
 
+		private string ValidateData()
+		{
+			string output = "";
+
+			double teamOneScore = 0;
+			double teamTwoScore = 0;
+
+			bool scoreOneValid = double.TryParse(teamOneScoreValue.Text, out teamOneScore);
+			bool scoreTwoValid = double.TryParse(teamTwoScoreValue.Text, out teamTwoScore);
+
+			if (!scoreOneValid)
+			{
+				output = "The score One value is not a valid number.";
+			}
+			else if (!scoreTwoValid)
+			{
+				output = "The score Two value is not a valid number.";
+			}
+			else if (teamOneScore == 0 && teamTwoScore == 0)
+			{
+				output = "You did not enter a score for either team.";
+			}
+			else if (teamOneScore == teamTwoScore)
+			{
+				output = "We do not allow ties in this application.";
+			}
+
+			return output;
+		}
+
 		private void scoreButton_Click(object sender, EventArgs e)
 		{
+			string errorMessage = ValidateData();
+
+			if (errorMessage.Length > 0)
+			{
+				MessageBox.Show($"Input Error: { errorMessage }");
+				return;
+			}
+			
 			MatchupModel m = (MatchupModel)matchupListBox.SelectedItem;
 			double teamOneScore = 0;
 			double teamTwoScore = 0;
@@ -194,7 +232,15 @@ namespace TrackerUI
 				}
 			}
 
-			TournamentLogic.UpdateTournamentResults(tournament);
+			try
+			{
+				TournamentLogic.UpdateTournamentResults(tournament);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"The application had the following error: { ex.Message }");
+				return;
+			}
 
 			LoadMatchups((int)roundDropDown.SelectedItem);
 		}
